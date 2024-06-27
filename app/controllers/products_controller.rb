85f41b3a4323c5378180412b08426192bffcf592
Angel <anngelao25@gmail.com>
2024-06-26 21:03:59 -0500

    Polished the Notification Component

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,7 +1,8 @@
 class ProductsController < ApplicationController
   before_action :authenticate_user!
 
-  def index; end
+  def index
+  end
 
   def new
     @product = current_user.products.build
