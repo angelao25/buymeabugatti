6956c2ac5fd870098e43421b94fc84a35b1426c9
Angel <anngelao25@gmail.com>
2024-07-04 11:13:36 -0500

    Removed unnecessary timeouts in notification_controller

--- app/javascript/controllers/notification_controller.js
+++ app/javascript/controllers/notification_controller.js
@@ -5,11 +5,7 @@ import { enter, leave, toggle} from "el-transition";
 export default class extends Controller {
   connect() {
       setTimeout(() => {
-        this.element.classList.remove("hidden");
-
-        setTimeout(() => {
           enter(this.element);
-        }, 100);
       }, 500);
 
     // Auto-hide
@@ -19,9 +15,7 @@ export default class extends Controller {
   }
 
   close() {
-    setTimeout(() => {
-      leave(this.element)
-    }, 100);
+    leave(this.element)
 
     // Remove element after transition
     setTimeout(() => {
