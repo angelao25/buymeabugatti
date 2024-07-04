16eba1ca8a4e79a078da97fea96b0a61bc94dd2e
Angel <anngelao25@gmail.com>
2024-07-04 17:17:26 -0500

    Added interactive uploaded file component

---
+++ app/javascript/controllers/uploaded_file_controller.js
@@ -0,0 +1,27 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="uploaded-file"
+export default class extends Controller {
+  static targets = ["open", "close", "form"];
+
+  open(e){
+    e.preventDefault();
+    this.openTarget.classList.add("hidden");
+    this.closeTarget.classList.remove('hidden');
+    this.formTarget.classList.remove("hidden");
+  }
+
+  close(e){
+    e.preventDefault();
+    this.openTarget.classList.remove("hidden");
+    this.closeTarget.classList.add("hidden");
+    this.formTarget.classList.add("hidden");
+
+  }
+
+  delete(e) {
+    e.preventDefault();
+    this.element.remove();
+
+  }
+}
